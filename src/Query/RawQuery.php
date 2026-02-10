056d4d8ccc251925d501cb263cc12667c571386f
Jackardios <salahutdinov.salavat@gmail.com>
2026-02-10 20:13:46 +0300

    enforce that RawQuery cannot be constructed or reset with an empty array by throwing InvalidQueryException

--- src/Query/RawQuery.php
+++ src/Query/RawQuery.php
@@ -4,20 +4,30 @@ declare(strict_types=1);
 
 namespace Jackardios\EsScoutDriver\Query;
 
+use Jackardios\EsScoutDriver\Exceptions\InvalidQueryException;
+
 final class RawQuery implements QueryInterface
 {
     /** @var array<string, mixed> */
     private array $query;
 
     /** @param array<string, mixed> $query */
-    public function __construct(array $query = [])
+    public function __construct(array $query)
     {
+        if ($query === []) {
+            throw new InvalidQueryException('RawQuery requires a non-empty query array');
+        }
+
         $this->query = $query;
     }
 
     /** @param array<string, mixed> $query */
     public function query(array $query): self
     {
+        if ($query === []) {
+            throw new InvalidQueryException('RawQuery requires a non-empty query array');
+        }
+
         $this->query = $query;
         return $this;
     }
